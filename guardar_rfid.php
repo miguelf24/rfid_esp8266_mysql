52ff778acc7f0b26f16f60ea7c425cff042b2c75
miguelf24 <51495641+miguelf24@users.noreply.github.com>
2025-02-04 23:40:38 -0600

    Update guardar_rfid.php

--- guardar_rfid.php
+++ guardar_rfid.php
@@ -1,17 +1,10 @@
 <?php
 
-// $servername = getenv("MYSQLHOST") ?: "mysql.railway.internal";
-// $username   = getenv("MYSQLUSER") ?: "root";
-//  $password   = getenv("MYSQLPASSWORD") ?: "tu_contraseña_aquí";
-// $dbname     = getenv("MYSQL_DATABASE") ?: "railway";
+ $servername = getenv("MYSQLHOST") ?: "mysql.railway.internal";
+ $username   = getenv("MYSQLUSER") ?: "root";
+  $password   = getenv("MYSQLPASSWORD") ?: "tu_contraseña_aquí";
+ $dbname     = getenv("MYSQL_DATABASE") ?: "railway";
 
-
-
-// Intentar obtener variables con getenv()
-$servername = $_ENV["MYSQLHOST"] ?? "ERROR";
-$username   = $_ENV["MYSQLUSER"] ?? "ERROR";
-$password   = $_ENV["MYSQLPASSWORD"] ?? "ERROR";
-$dbname     = $_ENV["MYSQL_DATABASE"] ?? "ERROR";
 
 echo "Servidor: $servername<br>";
 echo "Usuario: $username<br>";
