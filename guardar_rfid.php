561b3f9bea71fe5db624fc19937d41684c6632ad
miguelf24 <51495641+miguelf24@users.noreply.github.com>
2025-02-04 23:05:51 -0600

    Update guardar_rfid.php

--- guardar_rfid.php
+++ guardar_rfid.php
@@ -5,7 +5,7 @@ $username   = getenv("DB_USER");
 $password   = getenv("DB_PASSWORD");
 $dbname     = getenv("DB_NAME");
 $port       = getenv("DB_PORT");
- 
+ echo "$servername <br>";
 // Verificar si las variables existen
 if (!$servername || !$username || !$password || !$dbname || !$port) {
     die("❌ Error: Variables de entorno no definidas.");
