09fbec7635ab41e8f63e617c5e941ac51aada770
miguelf24 <51495641+miguelf24@users.noreply.github.com>
2025-02-04 23:44:35 -0600

    Update guardar_rfid.php

--- guardar_rfid.php
+++ guardar_rfid.php
@@ -1,15 +1,14 @@
 <?php
 
- $servername = getenv("MYSQLHOST") ;
- $username   = getenv("MYSQLUSER") ;
-  $password   = getenv("MYSQLPASSWORD");
- $dbname     = getenv("MYSQL_DATABASE");
-
+$servername = getenv("MYSQLHOST");
+$username   = getenv("MYSQLUSER");
+$password   = getenv("MYSQLPASSWORD");
+$dbname     = getenv("MYSQL_DATABASE");
 
 echo "Servidor: $servername<br>";
 echo "Usuario: $username<br>";
 echo "Contraseña: $password<br>";
 echo "Base de datos: $dbname<br>";
 
-
 ?>
+
