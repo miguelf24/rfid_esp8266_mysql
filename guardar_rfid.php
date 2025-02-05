8269b09057554ff0ad518de02d4860ef3a7f6c17
miguelf24 <51495641+miguelf24@users.noreply.github.com>
2025-02-04 23:26:16 -0600

    Update guardar_rfid.php

--- guardar_rfid.php
+++ guardar_rfid.php
@@ -1,14 +1,16 @@
 <?php
-$servername = $_getenv("DB_SERVER") ?? "NO DEFINIDO";
-$username   = $_getenv("DB_USER") ?? "NO DEFINIDO";
-$password   = $_getenv("DB_PASSWORD") ?? "NO DEFINIDO";
-$dbname     = $_getenv("DB_NAME") ?? "NO DEFINIDO";
-$port       = $_getenv("DB_PORT") ?? "NO DEFINIDO";
+$servername = getenv("DB_SERVER") ?? "NO DEFINIDO";
+$username   = getenv("DB_USER") ?? "NO DEFINIDO";
+$password   = getenv("DB_PASSWORD") ?? "NO DEFINIDO";
+$dbname     = getenv("DB_NAME") ?? "NO DEFINIDO";
+$port       = getenv("DB_PORT") ?? "NO DEFINIDO";
 
-echo "Servidor: $servername<br>";
-echo "Usuario: $username<br>";
-echo "Contraseña: $password<br>";
-echo "Base de datos: $dbname<br>";
-echo "Puerto: $port<br>";
+// Intenta conectarte a la base de datos
+try {
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa";
+} catch(PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
 ?>
-
