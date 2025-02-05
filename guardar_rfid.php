028611fa4766b5a01237da7cb554a875acb2a79e
miguelf24 <51495641+miguelf24@users.noreply.github.com>
2025-02-04 23:20:13 -0600

    Update guardar_rfid.php

--- guardar_rfid.php
+++ guardar_rfid.php
@@ -1,9 +1,9 @@
 <?php
-$servername = $_getenv["DB_SERVER"] ?? "NO DEFINIDO";
-$username   = $_getenv["DB_USER"] ?? "NO DEFINIDO";
-$password   = $_getenv["DB_PASSWORD"] ?? "NO DEFINIDO";
-$dbname     = $_getenv["DB_NAME"] ?? "NO DEFINIDO";
-$port       = $_getenv["DB_PORT"] ?? "NO DEFINIDO";
+$servername = $_getenv("DB_SERVER") ?? "NO DEFINIDO";
+$username   = $_getenv("DB_USER") ?? "NO DEFINIDO";
+$password   = $_getenv("DB_PASSWORD") ?? "NO DEFINIDO";
+$dbname     = $_getenv("DB_NAME") ?? "NO DEFINIDO";
+$port       = $_getenv("DB_PORT") ?? "NO DEFINIDO";
 
 echo "Servidor: $servername<br>";
 echo "Usuario: $username<br>";
